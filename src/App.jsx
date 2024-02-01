650b77d5e54d17db6c86aea65a5c9c4b526b8679
agustintarancon <agustinbuf@gmail.com>
2024-02-01 10:28:36 -0300

    structure navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Error from "./pages/error/Error"
+import Navbar from "./components/general/navbar/Navbar"
 
 function App() {
+
+  
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" Component={Home}/>
         <Route path="*" Component={Error}/>
